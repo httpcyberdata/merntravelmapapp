2a313f8f36ac45dc4871bdb184a76fd8b7a93d35
httpcyberdata <shieldedsafety@gmail.com>
2023-02-28 16:29:14 -0500

    chore: added mongodb URL as an environment variable

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/merntravelmap', {useNewUrlParser: true})
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true})
 
 app.listen(8800, () => {
     console.log('Backend server is operating 🖥️')
