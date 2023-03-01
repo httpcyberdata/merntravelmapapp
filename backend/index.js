8045d448952a097035b8458664e0a51ef0ebf456
httpcyberdata <shieldedsafety@gmail.com>
2023-03-01 09:11:55 -0500

    chore: added models & routes to the app

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,23 @@ const mongoose = require('mongoose');
 const chalk = require('chalk');
 const dotenv = require('dotenv');
 const app = express();
+const pinRoute = require('./routes/pins');
+
 
 dotenv.config();
+
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL , {useNewUrlParser: true})
     .then(() => {
         console.log(chalk.blue('MongoDB') + chalk.cyanBright.underline.bold(' Connected! 🌐 '))
     })
+    .catch((err) => console.log(err));
+
+    app.use('/api/pins', pinRoute);
+    
+
+
 
 app.listen(8800, () => {
     console.log(chalk.magentaBright('Backend server') + chalk.redBright(' is operating 🖥️'));
